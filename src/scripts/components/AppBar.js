fab9faeb1d19a06fc8eeeeefce7499945886f65b
Ajijagatsaputra <ajijagat90@gmail.com>
2024-05-26 16:00:58 +0700

    belumdone

--- src/scripts/components/AppBar.js
+++ src/scripts/components/AppBar.js
@@ -1,16 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const appBar = document.createElement("header");
+document.addEventListener("DOMContentLoaded", () => {
+  const appBar = document.createElement("nav");
   appBar.innerHTML = `
-    <nav class = "hamburgermenu">
-      <div class="logo">
-        <div class="logo-text"></div>
-      </div>
-      <ul class="nav-links">
-        <li><a href="/">Home</a></li>
-        <li><a href="https://www.linkedin.com/in/aji-jagat-saputra-186b97227/">About us</a></li>
-        <li><a href="#">Favorite</a></li>
-      </ul>
-    </nav>
+    <div class="logo">
+      <div class="logo-text"></div>
+    </div>
+    <button class="hamburger">
+      <img src="../public/images/heros/hero-image_2.jpg" alt="">
+    </button>
   `;
   document.getElementById("app").appendChild(appBar);
 });
