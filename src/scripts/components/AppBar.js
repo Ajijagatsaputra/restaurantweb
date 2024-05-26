8ac7d3b2c1533c9c40cabcb071813568de164e4b
Ajijagatsaputra <ajijagat90@gmail.com>
2024-05-26 23:38:46 +0700

    belumnampilhamburgermenu

--- src/scripts/components/AppBar.js
+++ src/scripts/components/AppBar.js
@@ -7,6 +7,11 @@ document.addEventListener("DOMContentLoaded", () => {
     <button class="hamburger">
       <img src="../public/images/heros/hero-image_2.jpg" alt="">
     </button>
+    <ul class="nav-links">
+      <li><a href="/">Home</a></li>
+      <li><a href="https://www.linkedin.com/in/aji-jagat-saputra-186b97227/">About us</a></li>
+      <li><a href="#">Favorite</a></li>
+    </ul>
   `;
   document.getElementById("app").appendChild(appBar);
 });
