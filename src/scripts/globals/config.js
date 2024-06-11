5bdfdf3824e3dd1f59e7a61e6282c967ce21bcb7
Ajijagatsaputra <ajijagat90@gmail.com>
2024-06-11 22:54:02 +0700

    add to folder submission2

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+    BASE_URL: 'https://restaurant-api.dicoding.dev',
+    IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/small/',
+    LANGUAGE_DEFAULT: "en-us",
+    CACHE_NAME: "RestoKita-V1",
+    DATABASE_NAME: "RestoKita-database",
+    DATABASE_VERSION: 1,
+    OBJECT_STORE_NAME: "restaurant",
+  };
+  
+  export default CONFIG;
+  
